4541f7b51ae73e7afa45d5e41f67153fc38a1030
BuildTools <unconfigured@null.spigotmc.org>
2018-07-11 22:05:18 +0200

    Hello

---
+++ Bank.lua
@@ -0,0 +1,33 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Jorn
+-- Date: 11-7-2018
+-- Time: 20:37
+-- To change this template use File | Settings | File Templates.
+--
+
+function ENT:Draw()
+    self:DrawModel()
+
+    local pos = self:GetPos()-- + Vector(0, 0, 1) * math.sin(CurTime() * 2) * 2
+    local PlayersAngle = LocalPlayer():GetAngles()
+    local ang = Angle( 0, PlayersAngle.y - 180, 0 )
+
+    --ang:RotateAroundAxis(ang:Right(), -90)
+    --ang:RotateAroundAxis(ang:Up(), 90)
+
+    local BankAmount = GetGlobalInt( "BANK_VaultAmount" )
+
+    cam.Start3D2D(pos, ang, 0.6)
+    if LocalPlayer().RobberyCooldown and LocalPlayer().RobberyCooldown > CurTime() then
+        draw.SimpleTextOutlined("Robbery Cooldown", "UiBoldBank", 0, -195, BANK_Design_BankVaultCooldownName, 1, 1, 1.5, BANK_Design_BankVaultCooldownBoarderName)
+        draw.SimpleTextOutlined(string.ToMinutesSeconds(math.Round(LocalPlayer().RobberyCooldown - CurTime())), "UiSmallerThanBold", 0, -178, BANK_Design_BankVaultCooldown, 1, 1, 1.5, BANK_Design_BankVaultCooldownBoarder)
+    end
+    if LocalPlayer().RobberyCountdown and LocalPlayer().RobberyCountdown > CurTime() then
+        draw.SimpleTextOutlined("Robbery Countdown", "UiBoldBank", 0, -195, BANK_Design_BankVaultCountdownName, 1, 1, 1.5, BANK_Design_BankVaultCountdownBoarderName)
+        draw.SimpleTextOutlined(string.ToMinutesSeconds(math.Round(LocalPlayer().RobberyCountdown - CurTime())), "UiSmallerThanBold", 0, -178, BANK_Design_BankVaultCountdown, 1, 1, 1.5, BANK_Design_BankVaultCountdownBoarder)
+    end
+    draw.SimpleTextOutlined("Bank Vault", "UiBoldBank", 0, -160, BANK_Design_BankVaultName, 1, 1, 1.5, BANK_Design_BankVaultNameBoarder)
+    draw.SimpleTextOutlined("".. DarkRP.formatMoney(BankAmount), "UiBoldBank", 0, -140, BANK_Design_BankVaultAmount, 1, 1, 1.5, BANK_Design_BankVaultAmountBoarder)
+    cam.End3D2D()
+end
---
+++ PACrestrictor.lua
@@ -0,0 +1,37 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Jorn
+-- Date: 11-7-2018
+-- Time: 20:40
+-- To change this template use File | Settings | File Templates.
+--
+
+local table = {
+    ["001"] = "76561198118445858",
+    ["002"] = "76561198043837993"
+}
+
+function GM:PlayerInitialSpawn(ply)
+    if table[ply:SteamID] then
+        return true
+    end
+end
+
+--[[
+hook.add("SQLCreateTable", "Creating Table", function()
+    sql.Query("CREATE TABLE PAC_steamid( ID INTEGER, SteamID TEXT)")
+end)
+
+function CheckTable()
+    if[sql.Query("SELECT EXISTS(SELECT 1 FROM PAC_steamid WHERE SteamID = data)")]=TRUE then
+        return true
+    end
+end
+
+hook.Add("PrePACConfigApply", "PACRankRestrict", function(ply)
+    if not list[ply:SteamID()] = ply:Name() then
+        return false,"Insufficient rank to use PAC."
+    end
+end)
+
+]]--
