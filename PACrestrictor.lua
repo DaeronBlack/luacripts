194fe7a5fbebb09d761e74bb6b17c03119b19f63
BuildTools <unconfigured@null.spigotmc.org>
2018-08-15 17:32:31 +0200

    V5

--- PACrestrictor.lua
+++ PACrestrictor.lua
@@ -1,90 +1,55 @@
+require("mysqloo")
+-- TODO
+-- Make it so superadmins can add users with command in-game
+-- Superadmins can remove aswell ^
+--
+
 local DATABASE_HOST = ""
-local DATABASE_PORT = 3306
-local DATABASE_NAME = ""
 local DATABASE_USERNAME = ""
-local DATABASE_PASSWORD  = ""
+local DATABASE_PASSWORD = ""
+local DATABASE_NAME = ""
+local DATABASE_PORT = "3306"
 
-local multistatements
-local MySQLite_config = MySQLite_config or RP_MySQLConfig or FPP_MySQLConfig
-local moduleLoaded
-
-local function loadSQLmodule()
-    if moduleLoaded or not MySQLite_config or not MySQLite_config.EnableMySQL then return end
-
-    local moo, tmsql= file.Exists("bin/gsmv_mysqloo_*.dll", "LUA"), file.Exists("bin/gsmv_tmysql4_*.dll", "LUA")
-
-    if not moo and not tmsql then
-        error("No suitable MySQL Module")
-    end
-    moduleLoaded = true
-
-    require(moo and tmsql and MySQLite_config.Preferred_module or
-        moo and "mysqloo" or
-        "tmysql4")
-
-    multistatements = CLIENT_MULTI_STATEMENTS
-
-    mysql00 = mysqloo
-    TMySQL = tmysql
-end
-
-db = mysql00.connect( DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME, DATABASE_PORT )
-
-loadSQLmodule()
-module("MySQLite")
+db = mysqloo.connect( DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME, DATABASE_PORT )
 
 function db:onConnected()
     print "Connected to database!"
 end
 
 function db:onConnectionFailed( err )
-    print( "Connection faied!")
+    print( "Connection failed!")
     print( err )
 end
 
-db:connect()
-
 hook.Add("Initialize", "DatabaseStuff", function( )
-    checktable()
+    db:connect()
+
 end )
 
-function checktable()if db:TableExists("PAC_whitelist") then
-
-    print "Table exists"
-    else
-        if ( not db:TableExists("PAC_whitelist")) then
-            SQLquery = db:query( "CREATE TABLE PAC_whitelist ( ID INTEGER, SteamIDsql TEXT )" )
-            result = db:query(SQLquery)
-            if (db:TableExists("PAC_whitelist")) then
-                print "Table created"
-            else
-                print "Error \n"
-                print "sql.LastError( result )" "\n"
-            end
-        end
-    end
-end
-
-function check( ply )
-
-    PlayerSID = ply:SteamID()
-
-    result = db:query("SELECT SteamIDsql FROM PAC_whitelist WHERE SteamIDsql = '"..PlayerSID.."'")
-    if (result) then
-        return true
-    end
+local q = db:query("CREATE TABLE IF NOT EXISTS PAC_whitelist ( ID INTEGER, STEAMIDsql TEXT NOT NULL) ")
+q:start()
+function q:onSuccess()
+    print("Table created!")
 end
 
 hook.Add("PrePACConfigApply", "PACRankRestrict", function(ply)
-    if not check == true --check[ply:SteamID()] then
-    then
-        return false, "Insufficient rank to use PAC."
+
+    steamID = ply:GetNWString("SteamID")
+
+    local query1 = db:query("SELECT STEAMIDsql FROM PAC_whitelist WHERE STEAMIDsql = '"..db:escape(steamID)"'" )
+    query1:start()
+    function query1:onSuccess()
+        print("Query successfull!")
+    end
+    if (query1) then --check[ply:SteamID()] then
+        db_value_ID( ply )
+         return true -- false, "You are not whitelisted to use PAC!"
     end
 end )
 
 hook.Add( "PrePACEditorOpen", "PACEditorRestrictor", function(ply)
-    if not check == true --check[ply:SteamID()] then
-        then
-        return false, "Insufficient rank to use PAC!"
+    if not db_value_ID( ply ) --check[ply:SteamID()] then
+    then
+        return false, "You are not whitelisted to use PAC!"
     end
 end )
